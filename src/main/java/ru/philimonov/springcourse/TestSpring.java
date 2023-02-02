7798adf1ba67ac2e84d955d03cdf3200c7b13e44
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-02 16:22:34 +0300

    add applicationContext.xml, TestSpring.java

---
+++ src/main/java/ru/philimonov/springcourse/TestSpring.java
@@ -0,0 +1,12 @@
+package ru.philimonov.springcourse;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        TestBean bean = context.getBean("testBean", TestBean.class);
+        System.out.println(bean.getName());
+        context.close();
+    }
+}
