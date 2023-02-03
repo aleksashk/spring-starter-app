f99336a8ea0914e28f3b279947d8b8edc408eca1
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-03 15:54:32 +0300

    scope Singleton

--- src/main/java/ru/philimonov/springcourse/TestSpring.java
+++ src/main/java/ru/philimonov/springcourse/TestSpring.java
@@ -5,11 +5,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println("musicPlayer2 == musicPlayer1 -> " + (musicPlayer2 == musicPlayer1));
+        System.out.println(musicPlayer1);
+        System.out.println(musicPlayer2);
 
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
