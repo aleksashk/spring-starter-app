65ecb98bda67d5afbd80931e52c05892e8bce52d
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-03 15:19:30 +0300

    Dependency injection with setter and simple value

--- src/main/java/ru/philimonov/springcourse/MusicPlayer.java
+++ src/main/java/ru/philimonov/springcourse/MusicPlayer.java
@@ -2,6 +2,8 @@ package ru.philimonov.springcourse;
 
 public class MusicPlayer{
     private Music music;
+    private String name;
+    private int volume;
 
     public MusicPlayer() {
     }
@@ -17,5 +19,25 @@ public class MusicPlayer{
 
     public void setMusic(Music music) {
         this.music = music;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
