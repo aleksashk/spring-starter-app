da953c4382b06833ee11ce0ebabb8939b962d9e9
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-03 14:30:34 +0300

    Inversion of Control with constructor

---
+++ src/main/java/ru/philimonov/springcourse/RockMusic.java
@@ -0,0 +1,7 @@
+package ru.philimonov.springcourse;
+
+public class RockMusic implements Music{
+    public String getSong() {
+        return "Wind cries Mary.";
+    }
+}
--- src/main/java/ru/philimonov/springcourse/TestSpring.java
+++ src/main/java/ru/philimonov/springcourse/TestSpring.java
@@ -5,8 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        TestBean bean = context.getBean("testBean", TestBean.class);
-        System.out.println(bean.getName());
+        Music music = context.getBean("musicBean", ClassicalMusic.class);
+        MusicPlayer player = new MusicPlayer(music);
+        player.playMusic();
         context.close();
     }
 }
