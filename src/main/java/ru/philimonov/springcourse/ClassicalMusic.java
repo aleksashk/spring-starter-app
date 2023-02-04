3366344bf2faa4147efe8108bfa7c81aed07317c
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-04 15:53:59 +0300

    init-method destroy-method factory-method

--- src/main/java/ru/philimonov/springcourse/ClassicalMusic.java
+++ src/main/java/ru/philimonov/springcourse/ClassicalMusic.java
@@ -1,6 +1,14 @@
 package ru.philimonov.springcourse;
 
 public class ClassicalMusic implements Music{
+
+    private ClassicalMusic(){}
+
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+
+    @Override
     public String getSong() {
         return "Hungarian Rhapsody.";
     }
