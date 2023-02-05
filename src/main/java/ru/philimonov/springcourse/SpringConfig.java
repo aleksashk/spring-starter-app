96f9fbbe45d1f173898a554f8bf714fa7e9513ae
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-05 14:27:21 +0300

    java code with annotations

---
+++ src/main/java/ru/philimonov/springcourse/SpringConfig.java
@@ -0,0 +1,21 @@
+package ru.philimonov.springcourse;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("ru.philimonov.springcourse")
+@PropertySource("classpath:musicPlayer.properties")
+public class SpringConfig {
+    @Bean
+    public ClassicalMusic classicalMusic() {
+        return new ClassicalMusic();
+    }
+
+    @Bean
+    public MusicPlayer player() {
+        return new MusicPlayer(classicalMusic());
+    }
+}
--- src/main/java/ru/philimonov/springcourse/TestSpring.java
+++ src/main/java/ru/philimonov/springcourse/TestSpring.java
@@ -1,15 +1,13 @@
 package ru.philimonov.springcourse;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
-        System.out.println(classicalMusic1);
+        ClassicalMusic classicalMusic2 = context.getBean("classicalMusic", ClassicalMusic.class);
+        System.out.println("classicalMusic1 = classicalMusic2: " + (classicalMusic1 == classicalMusic2));
         context.close();
     }
 }
