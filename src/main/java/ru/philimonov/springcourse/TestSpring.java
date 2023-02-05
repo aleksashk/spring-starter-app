a978980025e9104265a7fdbda692aa3364bd71f8
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-05 12:44:51 +0300

    @Autowired with inner dependency

--- src/main/java/ru/philimonov/springcourse/TestSpring.java
+++ src/main/java/ru/philimonov/springcourse/TestSpring.java
@@ -5,8 +5,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
-        musicPlayer.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
         context.close();
     }
 }
