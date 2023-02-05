4e8b11ccd2558af85278cfd228d4ea1a84a84c84
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-05 12:09:29 +0300

    Annotations @Component without id RockMusic

--- src/main/java/ru/philimonov/springcourse/TestSpring.java
+++ src/main/java/ru/philimonov/springcourse/TestSpring.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = context.getBean("classicalMusic", Music.class);
+        Music music = context.getBean("rockMusic",Music.class);
         MusicPlayer musicPlayer = new MusicPlayer(music);
         musicPlayer.playMusic();
         context.close();
