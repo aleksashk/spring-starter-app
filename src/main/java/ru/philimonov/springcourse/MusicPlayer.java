5a5ab50792f97cc068492d3cbc0b1fa9f90a732e
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-05 12:58:02 +0300

    @Autowired, @Qualifier

--- src/main/java/ru/philimonov/springcourse/MusicPlayer.java
+++ src/main/java/ru/philimonov/springcourse/MusicPlayer.java
@@ -1,21 +1,16 @@
 package ru.philimonov.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer{
-
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-    }
+    @Qualifier("classicalMusic")
+    private Music music;
 
     public String playMusic(){
-        return "Playing: " + classicalMusic.getSong() + " Playing: " + rockMusic.getSong();
+        return "Playing: " + music.getSong();
     }
 }
